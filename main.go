c3f013c8aa39ce83352050c3bf4e7e7855c2b718
Tomiwa <calebomoniyitomiwa@gmail.com>
2022-11-13 23:39:33 +0100

    completed all functions

--- main.go
+++ main.go
@@ -20,13 +20,13 @@ type MongoInstance struct {
 var ng MongoInstance
 
 const dbName = "fiber-hrms"
-const mongoURI = "mongodb://localhost:27017" + dbName
+const mongoURI = "mongodb://localhost:27017/" + dbName
 
 type Employee struct {
-	ID 		string	`json: "id, omitempty" bson:"_id, omitempty"`
-	Name 	string	`json: "name"`
-	Salary 	float64	`json: "salary"`
-	Age 	float64	`json: "age"`
+	ID     string  `json:"id,omitempty" bson:"_id,omitempty"`
+	Name   string  `json:"name"`
+	Salary float64 `json:"salary"`
+	Age    float64 `json:"age"`
 }
 
 func Connect() error{
@@ -97,6 +97,64 @@ func main() {
 
 	})
 
-	app.Put("/employee:id")
-	app.Delete("/employee:id")
+	app.Put("/employee/:id", func(c *fiber.Ctx) error{
+		ParamID := c.Params("id")
+
+		employeeID, err := primitive.ObjectIDFromHex(ParamID)
+
+		if err != nil {
+			return c.SendStatus(400)
+		}
+
+		employee := new(Employee)
+
+		if err := c.BodyParser(employee); err != nil {
+			return c.Status(400).SendString(err.Error())
+		}
+		query := bson.D{{Key:"_id", Value: employeeID}}
+		update := bson.D{ 
+			{Key: "&set",
+			Value: bson.D{ 
+				{Key: "name", Value: employee.Name},
+				{Key: "age", Value: employee.Age},
+				{Key: "salary", Value: employee.Salary},
+			},
+				
+		},
+	    }
+
+		ng.Db.Collection("employees").FindOneAndUpdate(c.Context(), query, update).Err()
+
+		if err != nil {
+			if err == mongo.ErrNoDocuments{
+				return c.SendStatus(400)
+
+			} 
+			return c.SendStatus(500)
+		}
+		employee.ID = ParamID
+		return c.Status(200).JSON(employee)
+	})
+
+
+	app.Delete("/employee/:id", func(c *fiber.Ctx) error{
+		
+		employeeID, err := primitive.ObjectIDFromHex(c.Params("id"))
+		if err != nil {
+			return c.SendStatus(400)
+		}
+
+		query := bson.D{{Key: "_id", Value: employeeID}}
+		result, err := ng.Db.Collection("employees").DeleteOne(c.Context(), &query)
+		if err != nil {
+			return c.SendStatus(500)
+		}
+		if result.DeletedCount <1 {
+			return c.SendStatus(404)
+		}
+		return c.Status(200).JSON("record has been deleted")
+	})
+
+
+	log.Fatal(app.Listen(":3000"))
 }
